3b145c29914b2cb0a3c0ee76f37b178f067f0197
luiz <luizmarcelolm@hotmail.com>
2023-11-19 12:04:36 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Site_Venda_Lanche.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
