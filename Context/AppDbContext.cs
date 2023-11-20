5b4778175767cab2477592e9c6f8764a0715cb1c
luiz <luizmarcelolm@hotmail.com>
2023-11-20 11:50:53 -0300

    Feita migration e inserindo dados no banco

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Site_Venda_Lanche.Models;
+
+namespace Site_Venda_Lanche.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set;}
+    }
+}
