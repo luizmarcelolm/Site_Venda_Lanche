a714fb76763e635faec16ccd341e3fcdcd9e75ae
luiz <luizmarcelolm@hotmail.com>
2023-12-09 19:37:58 -0300

    criando checkout

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site_Venda_Lanche.Models;
+using Site_Venda_Lanche.Repositories.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace Site_Venda_Lanche.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
